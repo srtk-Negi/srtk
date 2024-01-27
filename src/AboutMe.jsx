10397101ea7f0ad890728d3cd3cc0041dfd2dfa7
Sarthak <srnegi141@gmail.com>
2024-01-27 16:17:29 -0600

    added main components

---
+++ src/AboutMe.jsx
@@ -0,0 +1,18 @@
+export default function AboutMe() {
+  return (
+    <div id="aboutMe">
+      <h1>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero excepturi
+        beatae minima fugiat accusamus odit itaque minus sit, cumque a saepe
+        necessitatibus dolorum modi consectetur nobis omnis in provident
+        possimus.
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat est
+        minus amet, sapiente incidunt hic laborum tempore dolor maxime,
+        accusamus sequi libero dicta cumque labore molestias veritatis unde.
+        Aperiam, quis.
+      </h1>
+    </div>
+  );
+}
---
+++ src/Resume.jsx
@@ -0,0 +1,18 @@
+export default function Resume() {
+  return (
+    <div id="resume">
+      <h1>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit eaque quis
+        dolorum? Quidem, unde minus molestias autem corrupti quos dignissimos
+        veniam perspiciatis maxime asperiores et consequuntur magnam beatae sunt
+        iste.
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto
+        consequuntur magni ad quas explicabo natus deserunt aliquid repellat,
+        quibusdam officia in fugiat culpa ipsam quam veritatis! Soluta impedit
+        optio illo!
+      </h1>
+    </div>
+  );
+}
