e21c3dd9e221211961bb2d439ca183e0c249ae0b
Sarthak <srnegi141@gmail.com>
2024-02-16 09:45:22 -0600

    style changes, added sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,32 @@
-import Header from "./Header";
-import AboutMe from "./AboutMe";
-import Projects from "./Projects";
-import Resume from "./Resume";
-import Footer from "./Footer";
-import ContactMe from "./ContactMe";
+// Code: Main App component
+
+// Importing components
+import Header from "./Components/Header";
+import AboutMe from "./Components/AboutMe";
+import Projects from "./Components/Projects";
+import Resume from "./Components/Resume";
+import Footer from "./Components/Footer";
+import ContactMe from "./Components/ContactMe";
+import React from "react";
+
+// Importing styles
+import "./scss/header.scss";
+import "./scss/aboutMe.scss";
+import "./scss/projects.scss";
+import "./scss/resume.scss";
+import "./scss/footer.scss";
+import "./scss/contactMe.scss";
 
 function App() {
     return (
-        <>
+        <div>
             <Header />
             <AboutMe />
             <Projects />
             <Resume />
-            <Footer />
             <ContactMe />
-        </>
+            <Footer />
+        </div>
     );
 }
 
