ea9b1edc7c9af7aee15d3257b3aa10770391622d
Sarthak <srnegi141@gmail.com>
2024-02-17 01:32:11 -0600

    style changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,8 +4,11 @@ import "../scss/home.scss";
 export default function Home() {
     return (
         <div className="home">
-            <h1>Home</h1>
-            <p>Welcome to my portfolio!</p>
+            <h1>Hey, I'm Sarthak Negi!</h1>
+            <h2>
+                A full stack developer with experience working with react,
+                python
+            </h2>
         </div>
     );
 }
