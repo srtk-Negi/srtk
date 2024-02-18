5c4f3e8651b1e9cc1d5cd7c0d9d6c1dc68184da3
Sarthak <srnegi141@gmail.com>
2024-02-18 02:31:54 -0600

    added tailwind

---
+++ src/Components/SideBar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function SideBar() {
+    return (
+        <div className="left-0 flex flex-col font-bold h-screen w-16 m-0 text-white">
+            <div>A</div>
+            <div>B</div>
+        </div>
+    );
+}
