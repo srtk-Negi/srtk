61332671e2c6467ed2c7ae11b44f53bf5e22070f
Sarthak <srnegi141@gmail.com>
2024-03-02 02:26:49 -0600

    resume complete for now

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -1,7 +1,24 @@
+import { Button } from "primereact/button";
+
 export default function Resume() {
-  return (
-    <div id="resume">
-      <h1>resume</h1>
-    </div>
-  );
+    return (
+        <div id="resume">
+            <iframe
+                src="src/assets/resume/Resume-SarthakNegi.pdf"
+                className="resumeFrame"
+                frameborder="0"
+            ></iframe>
+            <div className="button">
+                <a
+                    href="src/assets/resume/Resume-SarthakNegi.pdf"
+                    target="_blank"
+                    download={true}
+                >
+                    <Button rounded raised outlined>
+                        Download
+                    </Button>
+                </a>
+            </div>
+        </div>
+    );
 }
