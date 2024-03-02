010540edbc2218e1adb6542dae18c2d87bed1443
Sarthak <srnegi141@gmail.com>
2024-03-02 01:00:06 -0600

    projects page complete (for now)

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,27 +1,42 @@
 import React from "react";
 import { Button } from "primereact/button";
 
-function ProjectCard({ title, description, image, github }) {
+function ProjectCard(props) {
     return (
         <>
             <div className="project-card">
                 <div className="project-img">
-                    <img src={image} alt={title} />
+                    <img src={props.image} alt={props.title} />
                 </div>
                 <div className="details">
-                    <h2>{title}</h2>
-                    <p>{description}</p>
+                    <h2>{props.title}</h2>
+                    <p>{props.description}</p>
+                    {props.stackUsed && (
+                        <div className="stack">
+                            <h3>Stack Used:</h3>
+                            {props.stackUsed.map((stack, index) => {
+                                return (
+                                    <span
+                                        key={index}
+                                        className="stack-item text-highlight"
+                                    >
+                                        {stack}
+                                    </span>
+                                );
+                            })}
+                        </div>
+                    )}
                     <div className="button">
-                        <Button
-                            className="p-button git-btn"
-                            href={github}
-                            target="_blank"
-                            rounded
-                            raised
-                            outlined
-                        >
-                            GitHub
-                        </Button>
+                        <a href={props.github} target="_blank">
+                            <Button
+                                className="p-button git-btn"
+                                rounded
+                                raised
+                                outlined
+                            >
+                                GitHub
+                            </Button>
+                        </a>
                     </div>
                 </div>
             </div>
@@ -33,21 +48,40 @@ export default function Projects() {
     const projects = [
         {
             title: "Guardian Wheels",
-            description: "Project 1 description",
+            description:
+                "Guardian Wheels -An advanced car alarm system that uses AI to recognize when a burglary is in progress. Images (via internal dashcam and drone) & geo info are sent to the user and the police.",
             image: "src/assets/images/GWheels.png",
             github: "https://github.com/srtk-Negi/Guardian-Wheels",
+            stackUsed: ["Python", "FastAPI", "JavaScript", "HTML", "CSS"],
         },
         {
-            title: "Commentalyzer",
-            description: "Project 2 description",
-            image: "src/assets/images/defaultProj.png",
-            github: "https://github.com/srtk-Negi/commentalyzer",
+            title: "Workout Tracker",
+            description:
+                "A workout tracker web application that uses the Geolocation API to find your current location and helps you keep record of your runnning and cycling workouts.",
+            image: "src/assets/images/workTracker.png",
+            github: "https://github.com/srtk-Negi/Work-Tracker",
+            stackUsed: ["HTML", "CSS", "JavaScript"],
         },
         {
             title: "Personal Portfolio",
-            description: "Project 3 description",
+            description: "This is my personal portfolio website.",
             image: "src/assets/images/portfolio.png",
             github: "https://github.com/srtk-Negi/portfolio-website",
+            stackUsed: ["React", "SCSS"],
+        },
+        {
+            title: "Commentalyzer",
+            description:
+                "Fetches all the comments from a youtube video and performs sentiment analysis.Uses the YouTube API to get all the comments from the video the link was provided for. Uses the OpenAI API to access ChatGPT to perform sentiment analysis.",
+            image: "src/assets/images/defaultProj.png",
+            github: "https://github.com/srtk-Negi/commentalyzer",
+            stackUsed: [
+                "React",
+                "Node.js",
+                "Express",
+                "OpenAI API",
+                "YouTube API",
+            ],
         },
     ];
 
@@ -61,6 +95,7 @@ export default function Projects() {
                         description={project.description}
                         image={project.image}
                         github={project.github}
+                        stackUsed={project.stackUsed}
                     />
                 );
             })}
