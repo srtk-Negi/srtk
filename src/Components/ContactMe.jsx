2e74acc3d081992664c0586ba0f4bd5342741ee3
Sarthak <srnegi141@gmail.com>
2024-03-10 15:14:15 -0500

    contact me setup complete with emailjs

--- src/Components/ContactMe.jsx
+++ src/Components/ContactMe.jsx
@@ -3,44 +3,76 @@ import { InputTextarea } from "primereact/inputtextarea";
 import { Button } from "primereact/button";
 import { Card } from "primereact/card";
 import { Divider } from "primereact/divider";
-
+import emailjs from "@emailjs/browser";
+import { useRef } from "react";
 import "../assets/css/contactMe.css";
 
 export default function ContactMe() {
-    const sendMessage = () => {
-        alert("Message sent successfully");
+    const form = useRef();
+
+    const serviceId = process.env.REACT_APP_SERVICE_ID;
+    const templateId = process.env.REACT_APP_TEMPLATE_ID;
+    const publicKey = process.env.REACT_APP_PUBLIC_KEY;
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm(serviceId, templateId, form.current, {
+                publicKey: publicKey,
+            })
+            .then(
+                () => {
+                    alert("Message Sent!");
+                },
+                (error) => {
+                    alert("FAILED...", error.text);
+                }
+            );
     };
+
     return (
         <div className="contactMe">
             <Card className="contact-card">
-                <h1>Contact Me</h1>
-                <Divider />
-                <div className="p-fluid card-fields">
-                    <div className="p-field p-mt-2">
-                        <label htmlFor="name">Name</label>
-                        <InputText className="required" id="name" type="text" />
-                    </div>
-                    <div className="p-field p-mt-2">
-                        <label htmlFor="email">Email</label>
-                        <InputText id="email" type="text" />
-                    </div>
-                    <div className="p-field p-mt-2">
-                        <label htmlFor="message">Message</label>
-                        <InputTextarea id="message" type="text" rows={5} />
-                    </div>
-                    <div className="button">
-                        <div className="btnContainer">
-                            <Button
-                                rounded
-                                raised
-                                outlined
-                                onClick={sendMessage}
-                            >
-                                Send
-                            </Button>
+                <form ref={form} onSubmit={sendEmail}>
+                    <h1>Contact Me</h1>
+                    <Divider />
+                    <div className="p-fluid card-fields">
+                        <div className="p-field p-mt-2">
+                            <label htmlFor="name">Name</label>
+                            <InputText
+                                className="required"
+                                id="name"
+                                type="text"
+                                name="user_name"
+                            />
+                        </div>
+                        <div className="p-field p-mt-2">
+                            <label htmlFor="email">Email</label>
+                            <InputText
+                                id="email"
+                                type="text"
+                                name="user_email"
+                            />
+                        </div>
+                        <div className="p-field p-mt-2">
+                            <label htmlFor="message">Message</label>
+                            <InputTextarea
+                                id="message"
+                                type="text"
+                                rows={5}
+                                name="message"
+                            />
+                        </div>
+                        <div className="button">
+                            <div className="btnContainer">
+                                <Button rounded raised outlined type="submit">
+                                    Send
+                                </Button>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </form>
             </Card>
         </div>
     );
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+
+export default defineConfig(({ command, mode }) => {
+    const env = loadEnv(mode, process.cwd(), "");
+
+    return {
+        plugins: [react()],
+        define: {
+            "process.env": env, // Make environment variables available as process.env
+        },
+    };
+});
