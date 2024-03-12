0315904aa1f90e6cd349509d7d06dd09ddebe9e5
Sarthak <srnegi141@gmail.com>
2024-03-12 11:18:42 -0500

    errorPage added

--- src/Components/ErrorPage.jsx
+++ src/Components/ErrorPage.jsx
@@ -1,8 +1,16 @@
+import "../assets/css/errorPage.css";
+import sadMan from "/sadMan.svg";
+
 const ErrorPage = () => {
     return (
-        <div>
-            <h1>404</h1>
-            <p>Page not found</p>
+        <div id="errorPageContainer">
+            <div className="errorText">
+                <h1>404</h1>
+                <p>Page not found</p>
+            </div>
+            <div className="sadMan">
+                <img src={sadMan} alt="sad man" />
+            </div>
         </div>
     );
 };
