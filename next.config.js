9b34cff8c49c4bb48a846c3f1a3c0f35a9417c0f
Sarthak Negi <srnegi141@gmail.com>
2025-07-28 00:20:55 -0500

    added next config js

---
+++ next.config.js
@@ -0,0 +1,22 @@
+// next.config.js
+import path from "path";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "2c7eswxxwk.ufs.sh",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    return config;
+  },
+};
+
+module.exports = nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Hero from "@/components/Hero";
 import About from "@/components/About";
-import Projects from "@/components/Projetcs";
+import Projects from "@/components/Projects";
 import Resume from "@/components/Resume";
 import Contact from "@/components/Contact";
 
