80a41e502078a8de10137c013e9552150bd9e14a
Sarthak Negi <srnegi141@gmail.com>
2025-07-28 00:30:14 -0500

    updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
-// next.config.js
 import path from "path";
+import { fileURLToPath } from "url";
+
+// Emulate __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
