7cc7821ef9ff141af2a62fe969625ffa351ec6d9
Sarthak Negi <srnegi141@gmail.com>
2025-07-27 23:56:07 -0500

    Fix Webpack alias for @ path

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -11,6 +12,10 @@ const nextConfig: NextConfig = {
         pathname: "/**",
       },
     ],
+  },
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    return config;
   },
 };
 
